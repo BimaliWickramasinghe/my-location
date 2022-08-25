cd2675b2658642614055383e87204e9898f98f59
BimaliWickramasinghe <bimalicw@gmail.com>
2022-08-26 01:26:52 +0530

    Initialize map using openlayers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  interaction,layer,custom,control,
+  Interactions,Overlays,Controls,
+  Map,Layers,Overlay,Util
+}from "react-openlayers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map view={{center:[0,0],zoom:2}}>
+        <Layers>
+          <layer.Tile></layer.Tile>
+        </Layers>
+      </Map>
     </div>
   );
 }
